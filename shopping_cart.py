b2e6d2e5e9f873a5a1dd4f9847f7dab643c79a6b
mcoyne16 <77979084+mcoyne16@users.noreply.github.com>
2021-02-24 14:46:38 -0500

    separating the capture and display processes

--- shopping_cart.py
+++ shopping_cart.py
@@ -24,23 +24,31 @@ products = [
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 #print(products)
 
+################################
 #INPUTS CAPTURE
 total_price = 0
+selected_ids = []
 
 while True:
     selected_id = input("Please input a product identifier: ")
     if selected_id == "DONE":
         break
     else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + "" + str(matching_product["price"]))
+        selected_ids.append(selected_id)
 
+################################
 #OUTPUT DISPLAY
 
-print("TOTAL PRICE: " +str(total_price))
+#print(selected_ids)
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("SELECTED PRODUCT: " + matching_product["name"] + "" + str(matching_product["price"]))
+    
 
+
+print("TOTAL PRICE: " +str(total_price))
 
 
 #A grocery store name of your choice
